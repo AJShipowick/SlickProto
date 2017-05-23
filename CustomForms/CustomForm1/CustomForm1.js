a02950c5c87d0fe9901bca6b17bc3d52c11b7890
Adam S <adamshipowick84@gmail.com>
2017-05-23 16:06:09 -0500

    Update html and slider

--- CustomForms/CustomForm1/CustomForm1.js
+++ CustomForms/CustomForm1/CustomForm1.js
@@ -3,11 +3,20 @@
 Function to activate form button to open the slider.
 ------------------------------------------------------------
 */
-function open_panel() {
+function openSlick() {
+    
+    document.getElementById("slickContactForm").style.visibility = "visible";
+    document.getElementById("slickSlider").style.right = "-300px";
+
+    //document.getElementById("slickImage".style.right = "-1px";
+    
+
+    //document.getElementById("slickContactForm").scrollIntoView();
+
     slideIt();
-    var a = document.getElementById("sidebar");
-    a.setAttribute("id", "sidebar1");
-    a.setAttribute("onclick", "close_panel()");
+    // var a = document.getElementById("sidebar");
+    // a.setAttribute("id", "sidebar1");
+    // a.setAttribute("onclick", "close_panel()");
 }
 /*
 ------------------------------------------------------------
@@ -15,12 +24,20 @@ Function to slide the sidebar form (open form)
 ------------------------------------------------------------
 */
 function slideIt() {
-    var slidingDiv = document.getElementById("slider");
+    var slidingDiv = document.getElementById("slickSlider");
     var stopPosition = 0;
     if (parseInt(slidingDiv.style.right) < stopPosition) {
         slidingDiv.style.right = parseInt(slidingDiv.style.right) + 2 + "px";
         setTimeout(slideIt, 1);
     }
+
+    // var slidingImage = document.getElementById("slickImage");
+    // var stopPosition2 = 0;
+    // if (parseInt(slidingDiv.style.right) < slidingImage.style.right) {
+    //     slidingImage.style.right = parseInt(slidingImage.style.right) + 2 + "px";
+    //     setTimeout(slideIt, 1);
+    // }
+
 }
 /*
 ------------------------------------------------------------
