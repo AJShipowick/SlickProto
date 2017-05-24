fced5c06dd5e5ff5bf1f58b077993587cb0e3bcf
Adam <Adam@AirborneGamer.com>
2017-05-23 19:10:49 -0500

    slide in and out with pure JS.  Style changes for CSS.  Code cleanup.

--- CustomForms/CustomForm1/CustomForm1.js
+++ CustomForms/CustomForm1/CustomForm1.js
@@ -1,65 +1,34 @@
-/*
-------------------------------------------------------------
-Function to activate form button to open the slider.
-------------------------------------------------------------
-*/
-function openSlick() {
-    
-    document.getElementById("slickContactForm").style.visibility = "visible";
-    document.getElementById("slickSlider").style.right = "-300px";
 
-    //document.getElementById("slickImage".style.right = "-1px";
-    
+let slickSliderOpen = false;
 
-    //document.getElementById("slickContactForm").scrollIntoView();
+function slickSliderClicked() {
 
-    slideIt();
-    // var a = document.getElementById("sidebar");
-    // a.setAttribute("id", "sidebar1");
-    // a.setAttribute("onclick", "close_panel()");
+    document.documentElement.style.overflow = 'hidden';  // firefox, chrome
+    document.body.scroll = "no"; // ie only 
+
+    if (!slickSliderOpen) {
+        document.getElementById("slickContactForm").style.visibility = "visible";
+        document.getElementById("slickSlider").style.right = "-300px";
+        document.getElementById("slickImage").style.right = "340px";
+
+        showSlickSlider();
+
+    } else {
+        closeSlickSlider()
+    }
+    slickSliderOpen = !slickSliderOpen;
 }
-/*
-------------------------------------------------------------
-Function to slide the sidebar form (open form)
-------------------------------------------------------------
-*/
-function slideIt() {
+
+function closeSlickSlider() {
+    document.getElementById("slickContactForm").style.visibility = "hidden";
+    document.getElementById("slickImage").style.right = "0px";
+}
+
+function showSlickSlider() {
     var slidingDiv = document.getElementById("slickSlider");
     var stopPosition = 0;
     if (parseInt(slidingDiv.style.right) < stopPosition) {
-        slidingDiv.style.right = parseInt(slidingDiv.style.right) + 2 + "px";
-        setTimeout(slideIt, 1);
-    }
-
-    // var slidingImage = document.getElementById("slickImage");
-    // var stopPosition2 = 0;
-    // if (parseInt(slidingDiv.style.right) < slidingImage.style.right) {
-    //     slidingImage.style.right = parseInt(slidingImage.style.right) + 2 + "px";
-    //     setTimeout(slideIt, 1);
-    // }
-
-}
-/*
-------------------------------------------------------------
-Function to activate form button to close the slider.
-------------------------------------------------------------
-*/
-function close_panel() {
-    slideIn();
-    a = document.getElementById("sidebar1");
-    a.setAttribute("id", "sidebar");
-    a.setAttribute("onclick", "open_panel()");
-}
-/*
-------------------------------------------------------------
-Function to slide the sidebar form (slide in form)
-------------------------------------------------------------
-*/
-function slideIn() {
-    var slidingDiv = document.getElementById("slider");
-    var stopPosition = -342;
-    if (parseInt(slidingDiv.style.right) > stopPosition) {
-        slidingDiv.style.right = parseInt(slidingDiv.style.right) - 2 + "px";
-        setTimeout(slideIn, 1);
+        slidingDiv.style.right = parseInt(slidingDiv.style.right) + 5 + "px";
+        setTimeout(showSlickSlider, 1);
     }
 }
