76d13ed0358a1b01b6d6cef96e8396b7d4c53cf9
Adam S <adamshipowick84@gmail.com>
2017-05-23 21:13:38 -0500

    Updated position of slider to be fixed.  Set background color of form so text in background cannot be seen.  Updated overflow to suppress X on loading slider, then use parent after slider is shown.

--- CustomForms/CustomForm1/CustomForm1.js
+++ CustomForms/CustomForm1/CustomForm1.js
@@ -3,10 +3,8 @@ let slickSliderOpen = false;
 
 function slickSliderClicked() {
 
-    document.documentElement.style.overflow = 'hidden';  // firefox, chrome
-    document.body.scroll = "no"; // ie only 
-
     if (!slickSliderOpen) {
+            document.documentElement.style.overflowX = 'hidden';
         document.getElementById("slickContactForm").style.visibility = "visible";
         document.getElementById("slickSlider").style.right = "-300px";
         document.getElementById("slickImage").style.right = "340px";
@@ -15,6 +13,7 @@ function slickSliderClicked() {
 
     } else {
         closeSlickSlider()
+        document.documentElement.style.overflowX = 'inherit';
     }
     slickSliderOpen = !slickSliderOpen;
 }
@@ -28,7 +27,7 @@ function showSlickSlider() {
     var slidingDiv = document.getElementById("slickSlider");
     var stopPosition = 0;
     if (parseInt(slidingDiv.style.right) < stopPosition) {
-        slidingDiv.style.right = parseInt(slidingDiv.style.right) + 5 + "px";
+        slidingDiv.style.right = parseInt(slidingDiv.style.right) + 10 + "px";
         setTimeout(showSlickSlider, 1);
     }
 }
